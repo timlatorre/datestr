6b060a9023acfd7fbd4786ff9a21161664f2ebae
Tim LaTorre <timlatorre@gmail.com>
2013-02-09 20:20:23 -0500

    header and profile view refinements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,19 +2,12 @@ module ApplicationHelper
 
   def show_dynamic_links
     if @auth
-      link_to("Profile", profile_path) +
-      link_to("(edit)", profile_edit_path) +
-      link_to("Photos", photos_path) +
-      link_to("(add)", photos_new_path) +
-      link_to("Questions", questions_path) +
-      link_to("(add)", questions_new_path) +
-      link_to("Messages", messages_path) +
-      link_to("Browse", browse_path) +
-      link_to("Logout", logout_path)
+      link_to("Profile", profile_path, :class => 'four columns nav-btn') +
+      link_to("Messages", messages_path, :class => 'four columns nav-btn') +
+      link_to("Browse", browse_path, :class => 'four columns nav-btn')
     else
       link_to("Login", login_path) +
-      link_to("Register", register_path) +
-      link_to("About", about_path)
+      link_to("Register", register_path)
     end
   end
 end
